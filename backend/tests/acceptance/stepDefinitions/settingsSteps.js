3fd78f9c7351dd9136a454f2dbc06604bc8b3103
Kassim Mohamed <kassim91000@gmail.com>
2022-04-06 23:35:11 +0200

    tests settings

--- backend/tests/acceptance/stepDefinitions/settingsSteps.js
+++ backend/tests/acceptance/stepDefinitions/settingsSteps.js
@@ -3,23 +3,23 @@ const {Given, When, Then} = require('@cucumber/cucumber')
 // const { expect } = require("@playwright/test");
 
 When('je saisie ma biographie en respectant les normes', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator.error.toBeFalsy();
+    return 'passed';
 });
 
 When('je clique sur le bouton Update profile', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator('form >> button:has-text("Update Profile")').click();
+    return 'passed';
 });
 
 Then('ma biographie est bien modifiée', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator.error.toBeFalsy();
+    return 'passed';
 });
 
 When('je saisie ma biographie sans respecter les normes', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator.error.toBeTruthy();
+    return 'passed';
 });
 
 Then('ma biographie ne s\'est pas modifiée', function () {
@@ -28,23 +28,24 @@ Then('ma biographie ne s\'est pas modifiée', function () {
 });
 
 Given('je suis sur la page Settings', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await expect(page).toHaveURL("http://localhost:3000/settings")
+    return 'passed';
 });
 
 When('je saisie un nouveau mot de passe en respectant les normes', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator.error.toBeFalsy();
+    return 'passed';
 });
 
 When('je saisie le même mot de passe dans le champ de confirmation de mot de passe', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    const password = await page.selector.password
+    expect('password').toMatch('password');
+    return 'passed';
 });
 
 When('je clique sur le bouton Update password', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator('PasswordSettings >> button:has-text("Update Password")').click();
+    return 'passed';
 });
 
 Then('mon mot de passe est bien modifié', function () {
@@ -53,8 +54,8 @@ Then('mon mot de passe est bien modifié', function () {
 });
 
 When('je saisie un nouveau mot de passe sans repecter les normes', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator.error.toBeTruthy();
+    return 'passed';
 });
 
 Then('mon mot de passe ne s\'est pas modifié', function () {
@@ -63,16 +64,18 @@ Then('mon mot de passe ne s\'est pas modifié', function () {
 });
 
 When('je ne saisie pas le même mot de passe dans le champ de confirmation de mot de passe', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator.error.toBeTruthy();
+    return 'passed';
 });
 
 When('je saisie mon ancien mot de passe', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator.error.toBeTruthy();
+    return 'passed';
 });
 
 Then('je ne vois pas l\'onglet Settings', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    const Settings = await page.locator(
+    'sidebar >> item:has-text("Settings")');
+    await expect(Settings).toBeNull();
+    return 'passed';
 });
