c67f119774bb75f0a09dc686a585375ace3304be
Steven TAING <taing.steven@outlook.fr>
2022-04-06 23:40:46 +0200

    register

--- tests/acceptance/features/register.feature
+++ tests/acceptance/features/register.feature
@@ -14,19 +14,19 @@ Fonctionnalité: Register
 
         Scénario: Un utilisateur ne s'est pas inscrit car le nom d’utilisateur est déjà existant
             Étant donné que je saisis un nom d'utilisateur déjà existant
-            Et que je remplis correctement les autres champs du formulaire
+            Et que je remplis correctement les champs du formulaire autre que nom d'utilisateur
             Lorsque je clique sur le bouton Register
             Alors je ne suis pas inscrit
 
         Scénario: Un utilisateur ne s'est pas inscrit car l'email est déjà utilisé
             Étant donné que je saisis un email déjà existant
+            Et que je remplis correctement les champs du formulaire autre que email
             Lorsque je clique sur le bouton Register
             Alors je ne suis pas inscrit
 
         Scénario: Un utilisateur ne s'est pas inscrit car tous les champs ne sont pas remplis
             Étant donné que je n’ai pas remplis tous les champs du formulaire
-            Lorsque je clique sur le bouton Register
-            Alors je ne suis pas inscrit
+            Alors je ne peux pas cliquer sur le bouton Register
 
         Scénario: Un utilisateur ne s'est pas inscrit car des champs ne sont pas valides
             Étant donné que j’ai des champs invalides dans le formulaire
--- tests/acceptance/stepDefinitions/registerSteps.js
+++ tests/acceptance/stepDefinitions/registerSteps.js
@@ -3,8 +3,8 @@ const {Given, When, Then} = require('@cucumber/cucumber')
 const { expect } = require("@playwright/test");
 
 Then('je ne vois pas le bouton Register', async function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    const registerBtn = await page.locator('button:disabled")');
+    await expect(registerBtn).toBeTruthy();
 });
 
 Given('je suis sur la page Register sans être connecté', async function () {
@@ -25,37 +25,47 @@ Given('j’ai remplis tous les champs du formulaire en respectant les normes', a
 
 When('je clique sur le bouton Register', async function () {
     await Promise.all([
-        page.waitForNavigation(/*{ url: 'http://localhost:3000/feed' }*/),
         page.locator('text=Join usRegisterI already have an account >> button').click()
     ]);
 });
 
 Given('je saisis un nom d\'utilisateur déjà existant', async function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-});
-
-Given('je remplis correctement les autres champs du formulaire', async function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator('[placeholder="Username"]').fill('test');
 });
 
 Given('je saisis un email déjà existant', async function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator('[placeholder="Email"]').fill('test@gmail.com');    
+});
+
+Given('je remplis correctement les champs du formulaire autre que nom d\'utilisateur', async function () {
+    await page.locator('[placeholder="Email"]').fill('test@test.ts');
+    await page.locator('[placeholder="Password"]').fill('password');
+    await page.locator('[placeholder="Confirm password"]').fill('password');
+});
+
+Given('je remplis correctement les champs du formulaire autre que email', async function () {
+    await page.locator('[placeholder="Username"]').fill('testbis');
+    await page.locator('[placeholder="Password"]').fill('password');
+    await page.locator('[placeholder="Confirm password"]').fill('password');
 });
 
 Given('je n’ai pas remplis tous les champs du formulaire', async function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator('[placeholder="Username"]').fill('test');
 });
 
 Given('j’ai des champs invalides dans le formulaire', async function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    await page.locator('[placeholder="Username"]').fill('testbis');
+    await page.locator('[placeholder="Email"]').fill('test@test.ts');
+    await page.locator('[placeholder="Password"]').fill('password');
+    await page.locator('[placeholder="Confirm password"]').fill('passwordd');
 });
 
 Then('je ne suis pas inscrit', async function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    const errorDiv = await page.locator('.error');
+    await expect(errorDiv).toBeTruthy();
+});
+
+Then('je ne peux pas cliquer sur le bouton Register', async function () {
+    const registerBtn = await page.locator('button:disabled")');
+    await expect(registerBtn).toBeTruthy();
 });
