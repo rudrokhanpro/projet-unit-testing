9c45e76832e61690b857b3dbcf7ee8dc8236b579
Steven TAING <taing.steven@outlook.fr>
2022-04-06 17:16:45 +0200

    add cucumberjs

---
+++ backend/tests/acceptance/stepDefinitions/registerSteps.js
@@ -0,0 +1,58 @@
+const {Given, When, Then} = require('@cucumber/cucumber')
+
+// const { expect } = require("@playwright/test");
+
+Then('je ne vois pas le bouton Register', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('je suis sur la page Register sans être connecté', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('j’ai remplis tous les champs du formulaire en respectant les normes', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('je clique sur le bouton Register', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('je suis inscrit', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('je saisis un nom d\'utilisateur déjà existant', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('je remplis correctement les autres champs du formulaire', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('je saisis un email déjà existant', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('je n’ai pas remplis tous les champs du formulaire', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('j’ai des champs invalides dans le formulaire', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('je ne suis pas inscrit', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
